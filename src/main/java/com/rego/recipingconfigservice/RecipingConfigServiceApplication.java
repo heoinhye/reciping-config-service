91d959d3a888cfb02cf5598c7d30d1a98d81c776
christy <pepupx2@naver.com>
2025-04-27 14:06:57 +0900

    chore: 설정파일 관리 서비스 초기 설정

---
+++ src/main/java/com/rego/recipingconfigservice/RecipingConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package com.rego.recipingconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class RecipingConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipingConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rego/recipingconfigservice/RecipingConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rego.recipingconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipingConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
